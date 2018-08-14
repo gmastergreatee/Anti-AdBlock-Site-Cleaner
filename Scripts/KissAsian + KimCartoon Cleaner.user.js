c22470e74f4ddd8e5031bea44a306ebd093e331f
gmastergreatee <g77471164@gmail.com>
2018-08-14 13:40:58 +0530

    Updated KimCartoon cleaner

--- Scripts/KissAsian + KimCartoon Cleaner.user.js
+++ Scripts/KissAsian + KimCartoon Cleaner.user.js
@@ -1,14 +1,14 @@
 // ==UserScript==
 // @name         KissAsian + KimCartoon Cleaner
 // @namespace    https://github.com/gmastergreatee/Anti-AdBlock-Site-Cleaner
-// @version      0.1
+// @version      0.1.0.1
 // @description  try to take over the world!
 // @author       gmastergreatee
 // @include      *kissasian.sh*
 // @include      *kimcartoon.me*
 // @include      *yucloud.co/direct/embed.php*
 // @grant        none
-// @require      http://code.jquery.com/jquery-3.3.1.min.js
+// @require http://code.jquery.com/jquery-3.3.1.min.js
 // ==/UserScript==
 
 $(document).ready(function () {
@@ -30,9 +30,19 @@ $(document).ready(function () {
         } catch (err) {}
     }
 
+    function display(el) {
+        try {
+            var $elm = $(el);
+            if ($elm) {
+                $elm.css('display', 'inherit');
+            }
+        } catch (err) {}
+    }
+
     function doWork() {
         // write anything below to remove from the DOM
-        remove('iframe:not([allowfullscreen=true],#my_video_1,#ifrmVast),.mgbox,.divCloseBut,.kcAds1,#hideAds,.fluid_nonLinear_middle');
+        remove('iframe:not(#divContentVideo:first-child(),#my_video_1),.mgbox,.divCloseBut,.kcAds1,#hideAds,.fluid_nonLinear_middle');
+        display('#my_video_1');
     }
 
     doLoop(0, 200);
