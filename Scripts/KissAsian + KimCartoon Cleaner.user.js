6cdf1d0f9b80bc7a76ed76cac2a46799f6e1a6c8
gmastergreatee <g77471164@gmail.com>
2018-08-16 00:46:38 +0530

    Infinite load fix in KimCartoon & KissAsian

--- Scripts/KissAsian + KimCartoon Cleaner.user.js
+++ Scripts/KissAsian + KimCartoon Cleaner.user.js
@@ -1,12 +1,13 @@
 // ==UserScript==
 // @name         KissAsian + KimCartoon Cleaner
 // @namespace    https://github.com/gmastergreatee/Anti-AdBlock-Site-Cleaner
-// @version      0.1.0.4
+// @version      0.1.0.5
 // @description  try to take over the world!
 // @author       gmastergreatee
 // @include      *kissasian.sh*
 // @include      *kimcartoon.me*
 // @include      *yucloud.co/direct/embed.php*
+// @include      *mp4upload.com*
 // @grant        none
 // @require http://code.jquery.com/jquery-3.3.1.min.js
 // ==/UserScript==
@@ -50,9 +51,13 @@ $(document).ready(function () {
 
     function doWork() {
         // write anything below to remove from the DOM
-        remove('iframe:not(#divContentVideo>iframe:first-child()),.mgbox,.divCloseBut,#videoAd,.kcAds1,#hideAds,.fluid_nonLinear_middle');
+        remove('iframe:not([allowfullscreen=true]),.mgbox,.divCloseBut');
+        if (window.location.href.includes('mp4upload.com/'))
+            remove('#overlay');
+        if (window.location.href.includes('kimcartoon.me/'))
+            remove('.kcAds1,#hideAds');
         display('#divContentVideo>iframe:first-child(),video,#divContentVideo>div');
-        html('#divDownload', '<b>Use <a href="http://www.eagleget.com/">EagleGet</a>/<a href="https://offmp4.app/">OffMP4</a> to get download links</b>');
+        html('#divDownload', '<b>Use <a href="http://www.eagleget.com/">EagleGet</a>/<a href="https://9xbuddy.app/">9xbuddy</a> to get download links</b>');
     }
 
     doLoop(0, 200);
