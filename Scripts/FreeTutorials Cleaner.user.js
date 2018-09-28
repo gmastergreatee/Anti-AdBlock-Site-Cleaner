e227bfe8091794d7580469b07d8d8c729a0c5483
Rajarshi Vaidya aka gmastergreatee <g77471164@gmail.com>
2018-09-28 10:56:48 +0530

    Update FreeTutorials Cleaner.user.js

--- Scripts/FreeTutorials Cleaner.user.js
+++ Scripts/FreeTutorials Cleaner.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         FreeTutorials Cleaner
 // @namespace    https://github.com/gmastergreatee/Anti-AdBlock-Site-Cleaner
-// @version      0.1.0.1
+// @version      0.1.0.2
 // @description  try to take over the world!
 // @author       gmastergreatee
 // @include      *freetutorials.us*
