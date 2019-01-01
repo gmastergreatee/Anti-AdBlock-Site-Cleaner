5fb1e2177ba81535d7ed494c85814415a1a6051d
Rajarshi Vaidya aka gmastergreatee <g77471164@gmail.com>
2019-01-02 01:28:03 +0530

    Update FreeTutorials Cleaner.user.js

--- Scripts/FreeTutorials Cleaner.user.js
+++ Scripts/FreeTutorials Cleaner.user.js
@@ -1,20 +1,56 @@
 // ==UserScript==
 // @name         FreeTutorials Cleaner
 // @namespace    https://github.com/gmastergreatee/Anti-AdBlock-Site-Cleaner
-// @version      0.1.0.3
+// @version      0.1.0.4
 // @description  try to take over the world!
 // @author       gmastergreatee
 // @include      *freetutorials.us*
+// @include      *freetutorials.eu*
 // @grant        none
 // @require      http://code.jquery.com/jquery-3.3.1.min.js
 // ==/UserScript==
 
-(function () {
-    'use strict';
+$(document).ready(function () {
+    function doLoop(counter, maxCounter) {
+        setTimeout(function () {
+            if (counter < maxCounter) {
+                doWork();
+                doLoop(counter + 1, maxCounter);
+            }
+        }, 500)
+    }
 
-    var items = $('.readmore');
-    $.each(items, function (index, obj) {
-        $(obj).removeClass('readmore');
-        $(obj).removeClass('alignnone');
-    });
-})();
+    function remove(el) {
+        try {
+            var $elm = $(el);
+            if ($elm.length > 0) {
+                $elm.remove();
+            }
+        } catch (err) {}
+    }
+
+    function display(el) {
+        try {
+            var $elm = $(el);
+            if ($elm.length > 0) {
+                $elm.css('display', 'inherit');
+            }
+        } catch (err) {}
+    }
+
+    function html(el, txt) {
+        try {
+            var $elm = $(el);
+            if ($elm.length > 0) {
+                $elm.html(txt);
+            }
+        } catch (err) {}
+    }
+
+    function doWork() {
+        // write anything below to remove from the DOM
+        remove('.adsbygoogle');
+    }
+
+    doLoop(0, 200);
+});
